6f2979494d4b0dc7e65e03886405b668acbabf86
Jionee <dnjswldb98@naver.com>
2021-06-06 17:00:20 +0900

    add main

---
+++ src/main/java/com/konkuk/kuReal/Main/Application.java
@@ -0,0 +1,13 @@
+package com.konkuk.kuReal.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
